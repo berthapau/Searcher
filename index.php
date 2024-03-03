04e8fb35c57116f536473f020665e9584276ea77
berthapau <160972947+berthapau@users.noreply.github.com>
2024-03-03 19:01:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
-   <meta charset="UTF-8" />
+<meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/icon.svg" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
