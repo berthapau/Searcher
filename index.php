222cd4a252a188a012768cd15fa1301fedbaa882
berthapau <160972947+berthapau@users.noreply.github.com>
2024-03-03 19:03:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<meta charset="UTF-8" />
-<link rel="icon" type="image/svg+xml" href="/icon.svg" />
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <link rel="icon" href="/favicon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title>AdsPower</title>
+    <title>ADA Chaine</title>
     <script type="module" crossorigin src="/assets/index-b817cae7.js"></script>
     <link rel="stylesheet" href="/assets/index-c4ac0d52.css">
   </head>
