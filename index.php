7f6db82aa69ceda63062913de04e331b3479cb0e
berthapau <160972947+berthapau@users.noreply.github.com>
2024-03-03 19:04:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>ADA Chaine</title>
     <script type="module" crossorigin src="/assets/index-b817cae7.js"></script>
-    <link rel="stylesheet" href="/assets/index-.css">
+    <link rel="stylesheet" href="/assets/index.css">
   </head>
   <body>
     <div id="app"></div>
