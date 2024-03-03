5d9fd905bd600ff75a2b4fb9b0298e9d76c35266
berthapau <160972947+berthapau@users.noreply.github.com>
2024-03-03 19:01:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-Searcher index
- d=F*
-    <meta charset="UTF-8" />
+
+   <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/icon.svg" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
