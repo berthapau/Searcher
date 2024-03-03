f911762cfdd0255ff94d70a4a1033bf85d6fa5fd
berthapau <160972947+berthapau@users.noreply.github.com>
2024-03-03 19:03:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,19 @@
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.svg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>AdsPower</title>
+    <script type="module" crossorigin src="/assets/index-b817cae7.js"></script>
+    <link rel="stylesheet" href="/assets/index-c4ac0d52.css">
+  </head>
+  <body>
+    <div id="app"></div>
+    
+  </body>
+</html>
